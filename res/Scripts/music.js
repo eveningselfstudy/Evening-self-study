b2e63e1af87229ee2a3e9d1ecd7327ef7ea2a684
晚自习 <eveningselfstudy6225@outlook.com>
2026-08-30 14:34:46 +0800

    更新 music.js

--- res/Scripts/music.js
+++ res/Scripts/music.js
@@ -1,7 +1,7 @@
 /* ============================================================
    QQ音乐嵌入式播放器
-   通过 iframe 嵌入 music.163.com 外链播放器
-   歌曲ID配置在 Resources/Music/playlist.js 的 qqMusicIds 数组
+   通过 iframe 嵌入 y.qq.com 外链播放器
+   歌曲mid配置在 Resources/Music/playlist.js 的 qqMusicMids 数组
    ============================================================ */
 let musicIndex = 0;
 
@@ -15,27 +15,27 @@ function loadQQMusic() {
   var titleEl = document.getElementById("musicTitle");
   if (!wrap) return;
 
-  var songId = qqMusicIds[musicIndex % qqMusicIds.length];
+  var songMid = qqMusicMids[musicIndex % qqMusicMids.length];
   var song = playlist[musicIndex % playlist.length];
 
   if (titleEl && song) titleEl.textContent = song.title || "音乐";
 
-  /* QQ音乐外链播放器：type=2 单曲，height=66 标准高度 */
+  /* QQ音乐外链播放器 */
   wrap.innerHTML =
-    '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" ' +
-    'class="music-qq-iframe" ' +
-    'src="https://music.163.com/outchain/player?type=2&id=' + songId +
-    '&auto=0&height=66"></iframe>';
+    '<iframe frameborder="0" border="0" marginwidth="0" marginheight="0" ' +
+    'class="music-qq-iframe" scrolling="no" ' +
+    'src="https://y.qq.com/n/ryqq/songDetail/' + songMid +
+    '?songtype=0&iframe=1"></iframe>';
 }
 
 /* 上一首 */
 function prevMusic() {
-  musicIndex = (musicIndex - 1 + qqMusicIds.length) % qqMusicIds.length;
+  musicIndex = (musicIndex - 1 + qqMusicMids.length) % qqMusicMids.length;
   loadQQMusic();
 }
 
 /* 下一首 */
 function nextMusic() {
-  musicIndex = (musicIndex + 1) % qqMusicIds.length;
+  musicIndex = (musicIndex + 1) % qqMusicMids.length;
   loadQQMusic();
 }
